564382ccb3833727334301d0c3d2cdacb47cce95
Tsheltrim-Dema <03230364.gcbs@rub.edu.bt>
2023-10-29 07:38:45 -0700

    suffle of picture

--- game.py
+++ game.py
@@ -19,6 +19,27 @@ bg = pygame.image.load('people.jpg')
 bg_rect = bg.get.rect()
 bg_rect.topleft = (0, 0)
 
+rows = 3
+cols = 3
+num_cells = rows * cols 
+
+cell_width = window_width // rows 
+cell_height = window_height // cols 
+
+cells = []
+rand_indexes = list (range(0, num_cells))
+print(rand_indexes)
+print()
+for i in range (num_cells):
+    x = (i % rows) * cell_width 
+    y = (i // cols) * cell_height
+    rect = pygame.rect(x, y, cell_width, cell_height)
+    rand_pos = random.choice(rand_indexes)
+    rand_indexes.remove(rand_indexes)
+    cells.append({'rect': rect, 'border': WHITE, 'order': i, 'pos':rand_pos})
+
+    print(cells[i])
+
 running = True
 while running:
     # poll for events
@@ -29,7 +50,13 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("Black")
-    screen.blit(bg, bg_rect)
+
+    for i, val in enumerate(cells):
+        pos = cells[i]['pos']
+        img_area = pygame.Rect(cells[pos]['rect'].x, cells[pos]['rect'].y, cell_width, cell_height)
+        screen.blit(bg, cells[i]['rect'], img_area)
+
+    screen.blit(bg, bg_rect, (400, 300, 100, 100))
 
 
     # RENDER YOUR GAME HERE
