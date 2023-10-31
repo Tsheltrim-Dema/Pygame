ad0d8cb55281e897a1fec0135e056348c5880c7e
Tsheltrim-Dema <03230364.gcbs@rub.edu.bt>
2023-10-30 23:14:17 -0700

    swaping of the picture

--- game.py
+++ game.py
@@ -15,9 +15,10 @@ RED = (255, 0, 0)
 CRIMSON = (220, 0, 0,)
 ORGANGE = (255, 127, 0)
 
-bg = pygame.image.load('people.jpg')
+bg = pygame.image.load('elephant.jpg') 
 bg_rect = bg.get.rect()
 bg_rect.topleft = (0, 0)
+
 
 rows = 3
 cols = 3
@@ -47,6 +48,33 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = false 
+        if event_type == pygame.MOUSEBUTTONDOWN and event_button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+
+            for cell in cells:
+                rect = cell['rect']
+                order = cell['order']
+                if rect.collidepoint(mouse_pos):
+                    if not img_selected:
+                        img_selected:
+                        cell['border'] = RED
+                    else:
+                        current_img = cell
+                        if current_img['order'] != selected_img['order']:
+                            #swap the image
+                            temp = selected_img['pos']
+                            cells[selected_img['order']]['pos'] = cells[current_img['order']]['pos']
+                            cells[current_img['order']]['border'] = temp
+
+                            cells[selected_img['order']]['border'] = WHITE
+                            selected_img = None
+
+                            #check if the puzzul is sloved
+
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("Black")
@@ -55,6 +83,7 @@ while running:
         pos = cells[i]['pos']
         img_area = pygame.Rect(cells[pos]['rect'].x, cells[pos]['rect'].y, cell_width, cell_height)
         screen.blit(bg, cells[i]['rect'], img_area)
+        pygame.draw.rect(screen, cells[i]['border'])
 
     screen.blit(bg, bg_rect, (400, 300, 100, 100))
 
